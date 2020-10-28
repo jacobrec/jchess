5d55170e5f177bdc852559a0cf905b0fc2295b55
Jacob Reckhard <jacobrec@users.noreply.github.com>
2020-10-27 19:16:47 -0600

    removed test starting stat

--- main.ml
+++ main.ml
@@ -4,21 +4,5 @@ open Jchess
 let () = Printexc.record_backtrace true
 
 let () =
-  let test2 = [
-      "e4";"e5";
-      "Bc4";"f5";
-      "Qh5";"g6";
-      "Qg6";"Ke7";
-      "Qf5";"Ke8";
-      "Qe5";"Qe7";
-      "Bf7";"Kd8";
-      "a3";"Qh4";
-      "Nh3";"a6";
-      "Qd4";"a5";
-      "e5";"a4";
-      "e6";"Ra7";
-      "e7";"Ra6";
-    ] in
-
-  let g = Game.create_from_moves test2 in 
+  let g = Game.create () in 
   start_cmdline g
